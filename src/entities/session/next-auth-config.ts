114fb90217cd9cdfe3424d0f8609dfd74156b9a6
ArthurMur <archeeandrews@yandex.ru>
2024-02-14 22:02:43 +0300

    feat: add email auth

--- src/entities/session/next-auth-config.ts
+++ src/entities/session/next-auth-config.ts
@@ -1,5 +1,6 @@
 import { AuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import EmailProvider from 'next-auth/providers/email';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { dbClient } from '@/shared/lib/db';
 import { compact } from 'lodash-es';
@@ -11,10 +12,21 @@ export const nextAuthConfig: AuthOptions = {
   adapter: PrismaAdapter(dbClient) as AuthOptions['adapter'],
   // Поставщики аутентификации, созданные на основе приватной конфигурации Github
   providers: compact([
+    EmailProvider({
+      server: {
+        host: privateConfig.EMAIL_SERVER_HOST, // Хост сервера электронной почты
+        port: privateConfig.EMAIL_SERVER_PORT, // Порт сервера электронной почты
+        auth: {
+          user: privateConfig.EMAIL_SERVER_USER, // Пользователь сервера электронной почты
+          pass: privateConfig.EMAIL_SERVER_PASSWORD, // Пароль сервера электронной почты
+        },
+      },
+      from: privateConfig.EMAIL_FROM, // Электронный адрес отправителя
+    }),
     privateConfig.GITHUB_ID && // Проверка наличия идентификатора Github в приватной конфигурации
       privateConfig.GITHUB_SECRET && // Проверка наличия секретного ключа Github в приватной конфигурации
+      // Создание провайдера Github
       GithubProvider({
-        // Создание провайдера Github
         clientId: privateConfig.GITHUB_ID, // Идентификатор приложения Github
         clientSecret: privateConfig.GITHUB_SECRET, // Секретный ключ приложения Github
       }),
--- src/shared/config/private.ts
+++ src/shared/config/private.ts
@@ -2,10 +2,14 @@ import { z } from 'zod';
 
 // Схема данных для приватной конфигурации
 const privateConfigSchema = z.object({
-  // Поле GITHUB_ID должно быть строкой, опциональное
   GITHUB_ID: z.string().optional(),
-  // Поле GITHUB_SECRET должно быть строкой, опциональное
   GITHUB_SECRET: z.string().optional(),
+
+  EMAIL_SERVER_USER: z.string(),
+  EMAIL_SERVER_PASSWORD: z.string(),
+  EMAIL_SERVER_HOST: z.string(),
+  EMAIL_SERVER_PORT: z.string(),
+  EMAIL_FROM: z.string(),
 });
 
 // Парсинг приватной конфигурации из переменных среды с использованием заданной схемы данных
