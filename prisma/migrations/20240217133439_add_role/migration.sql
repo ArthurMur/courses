3e61c61b1614191e903d1dbfee2bae80d151707e
ArthurMur <archeeandrews@yandex.ru>
2024-02-17 16:43:34 +0300

    fix: re-created migration and removed unnecessary

--- prisma/migrations/20240217133439_add_role/migration.sql
+++ prisma/migrations/20240217133439_add_role/migration.sql
@@ -5,9 +5,12 @@
   - Made the column `email` on table `User` required. This step will fail if there are existing NULL values in that column.
 
 */
+-- CreateEnum
+CREATE TYPE "ROLE" AS ENUM ('ADMIN', 'USER');
+
 -- AlterTable
-ALTER TABLE "User" ALTER COLUMN "email" SET NOT NULL,
-ALTER COLUMN "role" SET DEFAULT 'USER';
+ALTER TABLE "User" ADD COLUMN     "role" "ROLE" NOT NULL DEFAULT 'USER',
+ALTER COLUMN "email" SET NOT NULL;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "VerificationToken_token_key" ON "VerificationToken"("token");
