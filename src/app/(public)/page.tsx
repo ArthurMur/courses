1e4c66a6d1f58240e2867d74272434b5f52f1772
ArthurMur <archeeandrews@yandex.ru>
2024-05-12 09:50:58 +0300

    feat: remove course list feature

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,11 +1,8 @@
-import { CoursesList } from '@/features/courses-list/courses-list';
-
 export const dynamic = 'force-dynamic';
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col p-8">
       <h1 className="text-3xl font-bold mb-10">Курсы</h1>
-      <CoursesList />
     </main>
   );
 }
